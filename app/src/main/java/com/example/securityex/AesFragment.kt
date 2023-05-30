c537fe0d85238a500eae0bd38e0b960f7dc27cd1
HYUNJUNEPARK <june2ac4dev@gmail.com>
2023-05-31 08:51:27 +0900

    Base Commit

---
+++ app/src/main/java/com/example/securityex/AesFragment.kt
@@ -0,0 +1,29 @@
+package com.example.securityex
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.interpolator.view.animation.FastOutLinearInInterpolator
+import com.example.securityex.databinding.FragmentAesBinding
+
+class AesFragment : Fragment() {
+    private var _binding: FragmentAesBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_aes, container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/securityex/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.securityex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.securityex.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.mainActivity = this@MainActivity
+
+        supportFragmentManager.beginTransaction().apply {
+            add(R.id.activity_main_ecdh_fragment_container, EcdhFragment())
+            add(R.id.activity_main_aes_fragment_container, AesFragment())
+            commit()
+        }
+    }
+}
